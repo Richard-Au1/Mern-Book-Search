e8aa988e126c1a57ca43317f3782704aefa542ad
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-12-14 01:57:15 -0700

    added in token authentification and to create a new client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,27 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+    <Navbar />
+    <Outlet />
+  </ApolloProvider>
   );
 }
 
